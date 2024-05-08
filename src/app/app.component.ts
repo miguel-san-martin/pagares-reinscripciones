241740f57ede79c6d9540d6e9a8c2cbfbe2ca204
Miguel Angel San Martin <miguel.sanmartin@iest.edu.mx>
2024-05-08 14:38:33 -0600

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink,RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MaterialModule } from './shared-material-module/material.module';
 import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MaterialModule,SharedModule,RouterLink, RouterOutlet,],
+  imports: [MaterialModule, SharedModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './shared/scss/custom-template-miguel-v2.scss'
+  styleUrl: './shared/scss/custom-template-miguel-v2.scss',
 })
 export class AppComponent {
+  readonly flag!: boolean;
 
-  constructor(){
-
-  }
-
+  constructor() {}
 
   title = 'Contraloría';
-
 }
--- src/app/shared/components/alert-dialog/alert-dialog.component.ts
+++ src/app/shared/components/alert-dialog/alert-dialog.component.ts
@@ -1,14 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'shrd-alert-dialog',
   templateUrl: './alert-dialog.component.html',
-  styleUrl: './alert-dialog.component.scss'
+  styleUrl: './../../scss/custom-template-miguel-v2.scss',
 })
 export class AlertDialogComponent {
   @Input() msj?: string = 'Undefinited';
-  @Output() actionYes:EventEmitter<any> = new EventEmitter();
-  @Output() actionNo:EventEmitter<any> = new EventEmitter();
-
-
+  @Output() actionYes: EventEmitter<any> = new EventEmitter();
+  @Output() actionNo: EventEmitter<any> = new EventEmitter();
 }
