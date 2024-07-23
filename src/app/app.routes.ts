ec29b9fe231842159bb51909c412d802f4fc8335
miguel-san-martin <160758616+miguel-san-martin@users.noreply.github.com>
2024-07-23 17:58:20 -0600

    avance escolar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,46 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
-  {
-    path: 'pagares',
-    loadChildren: () =>
-      import('./modules/pagares/pagares.module').then((m) => m.PagaresModule),
-  },
-  {
-    path: 'camp',
-    loadChildren: () =>
-      import('./modules/camping/camping.module').then((m) => m.CampingModule),
-  },
-  {
-    path: 'shirt-sale',
-    loadChildren: () =>
-      import('./modules/shirt-sale/shirt-sale.module').then(
-        (m) => m.ShirtSaleModule,
-      ),
-  },
-  {
-    path: 'module-closed',
-    loadComponent: () =>
-      import('./pages/modulo-cerrado/modulo-cerrado.component').then(
-        (c) => c.ModuloCerradoComponent,
-      ),
-  },
-  {
-    path: 'partidas-presupuestales',
-    loadComponent: () =>
-      import('./modules/partidas-presupuestales/pages/partidas-presupuestales-main-page/partidas-presupuestales-main-page.component').then(
-        (c) => c.PartidasPresupuestalesMainPageComponent,
-      ),
-  },
+ // {
+ //    path: 'pagares',
+ //    loadChildren: () =>
+ //      import('./modules/pagares/pagares.module').then((m) => m.PagaresModule),
+ //  },
+ //  {
+ //    path: 'camp',
+ //    loadChildren: () =>
+ //      import('./modules/camping/camping.module').then((m) => m.CampingModule),
+ //  },
+ //  {
+ //    path: 'shirt-sale',
+ //    loadChildren: () =>
+ //      import('./modules/shirt-sale/shirt-sale.module').then(
+ //        (m) => m.ShirtSaleModule,
+ //      ),
+ //  },
+ //  {
+ //    path: 'module-closed',
+ //    loadComponent: () =>
+ //      import('./pages/modulo-cerrado/modulo-cerrado.component').then(
+ //        (c) => c.ModuloCerradoComponent,
+ //      ),
+ //  },
+ //  {
+ //    path: 'partidas-presupuestales',
+ //    loadComponent: () =>
+ //      import('./modules/partidas-presupuestales/pages/partidas-presupuestales-main-page/partidas-presupuestales-main-page.component').then(
+ //        (c) => c.PartidasPresupuestalesMainPageComponent,
+ //      ),
+ //  },
+
+  { path: '**', redirectTo: 'config-escolares', pathMatch: 'full' },
   {
     path: 'config-escolares',
     loadComponent: () =>
       import('./modules/admin-config-configuracion/page-config/page-config.component').then(
-        (c) => c.PageConfigComponent,
-      ),
+        (c) => c.PageConfigComponent,)
+      // canActivate: [authGuard]
   },
   {
     path: 'module-closed',
