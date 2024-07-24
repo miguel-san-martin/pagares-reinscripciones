4fa1ffc4555538fa356e5cb6f23bcbe6d5b94e2f
miguel-san-martin <160758616+miguel-san-martin@users.noreply.github.com>
2024-07-23 18:40:28 -0600

    cherryPick

--- src/app/modules/admin-config-configuracion/page-config/page-config.component.ts
+++ src/app/modules/admin-config-configuracion/page-config/page-config.component.ts
@@ -1,37 +1,40 @@
 import {
-
   Component,
   ElementRef,
   inject,
   OnInit,
   ViewChild,
-  ViewRef,
 } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { MaterialModule } from '../../../shared-material-module/material.module';
 import { AdministraConfiguracionService } from '../administra-configuracion.service';
-import { delay, map } from "rxjs";
-import { error } from '@angular/compiler-cli/src/transformers/util';
+import { delay, map, tap } from "rxjs";
 import { CommonModule } from "@angular/common";
 
 interface listParameters {
+  concepto: string;
   idParametro: string;
   descripcion: string;
   parametroInt: string;
+  grado:string;
 }
+
 
 @Component({
   selector: 'app-page-config',
   standalone: true,
-  imports: [SharedModule, MaterialModule,CommonModule],
+  imports: [SharedModule, MaterialModule, CommonModule],
   templateUrl: './page-config.component.html',
   styleUrl: 'page-config.component.scss',
 })
 export class PageConfigComponent implements OnInit {
   private horariosExamenes = inject(AdministraConfiguracionService);
   @ViewChild('loadingScreen') loadingScreen!: ElementRef;
-  isChecked: any;
-  protected listConfigs: listParameters[] = [];
+  listResponse: listParameters[] = [];
+
+  profesionalList: listParameters[] = [];
+  bacherorList: listParameters[] = [];
+  listExtra: listParameters[] = [];
 
   ngOnInit(): void {
     this.loadList();
@@ -43,17 +46,47 @@ export class PageConfigComponent implements OnInit {
       .pipe(
         map((array) =>
           array.parametros.map((item: any) => ({
+            concepto: item.concepto,
             idParametro: Number(item.idParametro),
             descripcion: item.descripcion,
             parametroInt: Number(item.parametroInt),
+            grado: item.grado,
           })),
         ),
-        delay(250)
+        delay(250),
       )
       .subscribe(
         (response) => {
-          this.listConfigs = response;
-          console.log(this.listConfigs);
+          this.listResponse = response;
+          this.listResponse.push(
+            {grado:'Testeo',
+            descripcion:'iapisa',
+            parametroInt: '1',
+            concepto: 'F',
+            idParametro:'34'}
+          )
+
+          this.listResponse.forEach(
+            (item:listParameters) => {
+              switch (item.grado.toLowerCase()){
+                case('preparatoria'):
+                  this.bacherorList.push(item);
+                  break;
+                case ('profesional'):
+                  this.profesionalList.push(item);
+                  break;
+                default:
+                  this.listExtra.push(item)
+              }
+          }
+          )
+
+
+          // this.bacherorList = this.listResponse.filter(id => id.grado.toLowerCase() === 'preparatoria');
+          // this.profesionalList = this.listResponse.filter(id => id.grado.toLowerCase() === 'profesional');
+
+          console.log(this.profesionalList);
+          console.log(this.bacherorList);
         },
         (error) => {
           console.error(error);
@@ -64,13 +97,14 @@ export class PageConfigComponent implements OnInit {
       );
   }
 
+
   onChange(id: number, event: any) {
     this.loadingScreen.nativeElement.style.display = 'block';
     console.log(id, event.checked);
     this.horariosExamenes.patchListHorarios(id).subscribe(
       (response) => {
         console.log(response);
-        this.loadList()
+        this.loadList();
       },
       () => {},
       () => {},
