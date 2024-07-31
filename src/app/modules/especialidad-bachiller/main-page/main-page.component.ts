8c1975c2090b26d03d26a98fdfcda894ae64d5a6
miguel-san-martin <160758616+miguel-san-martin@users.noreply.github.com>
2024-07-30 18:52:07 -0600

    cambiando obs por async

--- src/app/modules/especialidad-bachiller/main-page/main-page.component.ts
+++ src/app/modules/especialidad-bachiller/main-page/main-page.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   Component,
   inject,
   OnInit,
@@ -12,6 +11,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MaterialModule } from '../../../shared-material-module/material.module';
 import { SharedModule } from '@shared/shared.module';
 import { SnackbarComponent } from '../../camping/components/snackbar/snackbar.component';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -20,44 +20,83 @@ import { SnackbarComponent } from '../../camping/components/snackbar/snackbar.co
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent
-  extends SnackbarComponent
-  implements OnInit, AfterViewInit
-{
-  dataSource!: MatTableDataSource<any>;
-  // protected dataSource!: MatTableDataSource<any>
+export class MainPageComponent extends SnackbarComponent implements OnInit {
+  dataSource!: MatTableDataSource<any>; // protected dataSource!: MatTableDataSource<any>
   displayedColumns!: any[];
-  ALUMNO: any[] = [];
-  // protected especialidades: WritableSignal<especialidades[]> = signal([]);
   protected especialidades: WritableSignal<especialidades[]> = signal([]);
   protected students: WritableSignal<any> = signal([]);
   private special = inject(EspecialityServicesAService);
 
   ngOnInit(): void {
-    this.special.getAllEspecialities().subscribe((r: especialidades[]) => {
-      this.displayedColumns = r.map((r) => r.abreviatura);
-      this.displayedColumns.unshift('alumno');
-
-      this.especialidades.set(r);
-      //this.dataSource = new MatTableDataSource(this.ALUMNO);
-
-      this.special.getAllStudents().subscribe((response: student[]) => {
-        this.students.set(new MatTableDataSource(response));
-        console.log('0data', this.students());
-      });
-    });
+    this.tryAsinc();
+    // this.special
+    //   .getAllEspecialities()
+    //   .pipe()
+    //   .subscribe((r: especialidades[]) => {
+    //     this.displayedColumns = r.map((r) => r.abreviatura);
+    //     this.displayedColumns.unshift('alumno');
+    //
+    //     this.especialidades.set(r);
+    //
+    //     this.special.getAllStudents().subscribe((response: student[]) => {
+    //       this.students.set(new MatTableDataSource(response));
+    //     });
+    //   });
   }
 
-  ngAfterViewInit() {
-    console.log();
+  async tryAsinc() {
+    try {
+      //PROMESAS
+      const alumnos: student[] = await firstValueFrom(
+        this.special.getAllStudents(),
+      );
+      const specialities: especialidades[] = await firstValueFrom(
+        this.special.getAllEspecialities(),
+      );
+
+      //ASIGNACIÓN
+      this.displayedColumns = specialities.map(
+        (row: especialidades) => row.abreviatura,
+      );
+      this.displayedColumns.unshift('alumno');
+
+      this.especialidades.set(specialities);
+      this.students.set(new MatTableDataSource(alumnos));
+    } catch (err) {
+      console.log(err);
+    }
+    // finally {
+    //   console.log('Carga completa');
+    // }
+  }
+
+  async modifySpeciality({ value }: any, id: number) {
+    try {
+      const response = await firstValueFrom(
+        this.special.patchSpeciality(value, id),
+      );
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+      this.errorSnackBar();
+      this.tryAsinc();
+      //window.location.reload();
+    }
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue: string = (event.target as HTMLInputElement).value;
+    this.students().filter = filterValue.trim().toLowerCase();
   }
 
   isSame(alumn: number, abre: number) {
     return alumn === abre;
   }
 
-  spech({ value }: any, id: number) {
+  //? Al tocar un radio button se envián un  radio button.
+  onChange({ value }: any, id: number) {
     this.openSnackBar();
+
     this.special.patchSpeciality(value, id).subscribe({
       next: (response) => {
         console.log(response);
@@ -70,12 +109,10 @@ export class MainPageComponent
     console.log(value, id);
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.students().filter = filterValue.trim().toLowerCase();
-    console.log(this.dataSource);
-  }
+  //? Metodo accionado para filtrar por nombre o idiest.
 }
+
+/// INTERFACES ///
 
 export interface especialidades {
   uuid: string;
