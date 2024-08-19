25edfca2422d242ee5d24ae118f7b7e732cf8bbf
miguel-san-martin <160758616+miguel-san-martin@users.noreply.github.com>
2024-08-19 14:26:50 -0600

    Cherry Pick

---
+++ src/app/modules/grupo-concurso-maestro/page-consulta/page-consulta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-consulta',
+  standalone: false,
+
+  templateUrl: './page-consulta.component.html',
+  styleUrl: './page-consulta.component.scss',
+})
+export class PageConsultaComponent {}
